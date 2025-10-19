afa57671393472e407a8045bbffc3d190790ad74
G00416271 <G00416271@atu.ie>
2025-10-19 16:02:33 +0000

    Removed nested repo and added lab_four as regular folder

---
+++ lab_four/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import Content from './components/content';
+import Header from './components/header';
+import Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Read from './components/read';
+import Create from './components/create';
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    {/* this is my comment. */}
+      <BrowserRouter>
+        <Navbar bg="primary" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path='/' element={<Content></Content>}></Route>
+          <Route path='/read' element={<Read />}></Route>
+          <Route path='/create' element={<Create />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )  
+}
+
+export default App
---
+++ lab_four/src/components/read.jsx
@@ -0,0 +1,52 @@
+import Movies from "./movies";
+import { useEffect, useState } from "react"; //import useffect
+import axios from "axios";
+
+function read(){
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        axios.get('PUT API HERE!!!!!')
+            .then((response) => {
+                setMovies(response.data.movies);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },  []);
+
+// const Read = () => {
+
+//     const data =
+//         [
+//             {
+//                 "Title": "Avengers: Infinity War",
+//                 "Year": "2018",
+//                 "imdbID": "tt4154756",
+//                 "Type": "movie",
+//                 "Poster": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+//             },
+//             {
+//                 "Title": "Captain America: Civil War",
+//                 "Year": "2016",
+//                 "imdbID": "tt3498820",
+//                 "Type": "movie",
+//                 "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+//             },
+//             {
+//                 "Title": "World War Z",
+//                 "Year": "2013",
+//                 "imdbID": "tt0816711",
+//                 "Type": "movie",
+//                 "Poster": "https://m.media-amazon.com/images/M/MV5BNDQ4YzFmNzktMmM5ZC00MDZjLTk1OTktNDE2ODE4YjM2MjJjXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+//             }
+//         ];
+    return (
+        <div>
+            <h1>Hello from Read component</h1>
+            <Movies myMovies={movies}></Movies>
+        </div>
+    );
+}
+
+export default read;
